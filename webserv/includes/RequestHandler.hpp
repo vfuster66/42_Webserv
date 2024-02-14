7c48f9c6ef998ccdf18e5c7622e8248ee08ae959
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2024-02-14 11:49:24 +0100

    Update RequestHandler.hpp

--- webserv/includes/RequestHandler.hpp
+++ webserv/includes/RequestHandler.hpp
@@ -30,13 +30,13 @@ class RequestHandler
 private:
     void parseRequestLine(const std::string& line, HttpRequest& request);
     void parseHeaders(std::istringstream& stream, HttpRequest& request);
+    void parseBody(std::istringstream& stream, HttpRequest& request);
     std::string determineMimeType(const std::string& filePath);
 
 public:
     HttpRequest parseRequest(const std::string& requestText);
     HttpResponse handleRequest(const HttpRequest& request);
 
-    // Plus de méthodes pour gérer les requêtes et les réponses...
 };
 
 #endif
