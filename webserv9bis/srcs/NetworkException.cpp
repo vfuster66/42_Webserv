f856a81d908c142a39fdc85afe388501d2273f7c
Virginie FUSTER PEREZ <124174795+vfuster66@users.noreply.github.com>
2024-03-14 11:10:36 +0100

    Delete webserv9bis directory

--- webserv9bis/srcs/NetworkException.cpp
+++
@@ -1,5 +0,0 @@
-#include "../includes/NetworkException.hpp"
-
-NetworkException::NetworkException(const std::string& msg, int code) : AppException(msg, code)
-{
-}
--- webserv9bis/www/errors/script.js
+++
@@ -1,5 +0,0 @@
-// Redirige vers la page d'accueil après 5 secondes
-setTimeout(function()
-{
-    window.location.href = '/';
-}, 5000);
